7bb701364e5fd549ce7268a31bf7dc2c0069175a
Quint0n01 <81411725+Quint0n01@users.noreply.github.com>
2021-08-18 09:51:57 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Registration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Vaccine Registration page.");
+            Console.WriteLine("Please fill in your details below:");
+            Console.WriteLine("");
+
+            Console.Write("Your ID: ");
+            string idn = Console.ReadLine();
+
+            Console.Write("Your First Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Your Surname: ");
+            string surname = Console.ReadLine();
+
+            Console.Write("Your Gender: ");
+            string gender = Console.ReadLine();
+
+            Console.Write("Your date of Birth: ");
+            string dob = Console.ReadLine();
+
+            Console.Write("Province: ");
+            string prov = Console.ReadLine();
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Summary of information submitted:");
+            Console.WriteLine("ID: " + idn);
+            Console.WriteLine("First Name: " + name);
+            Console.WriteLine("Surname: " + surname);
+            Console.WriteLine("Gender: " + gender);
+            Console.WriteLine("DOB: " + dob);
+            Console.WriteLine("Province: " + prov);
+        }
+    }
+}
